2fc53e6b8b2d4efd74e6cc684d42494fc2243040
judare <fosebadgame@gmail.com>
2024-04-23 21:53:28 -0500

    change config on agent

--- src/app/services/config.ts
+++ src/app/services/config.ts
@@ -1,12 +1,15 @@
 "use client";
 let env = "dev";
 
-if (typeof window !== "undefined" && window.location.hostname == "blokay.com") {
+if (
+  typeof window !== "undefined" &&
+  window.location.hostname == "app.blokay.com"
+) {
   env = "prod";
 }
 
 export const VERSION = "1.0.1";
 export const ENV = env;
-export const API_PROD = "https://blokay.com/api/";
+export const API_PROD = "https://app.blokay.com/api/";
 export const API_SANDBOX = "http://localhost:3000/api/";
 export const API_DEV = "http://localhost:3000/api/";
