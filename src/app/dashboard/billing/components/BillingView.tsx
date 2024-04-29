209cffd271207dfa45aacbe7a99933cf7b6bd09d
judare <fosebadgame@gmail.com>
2024-04-29 14:55:16 -0500

    fix fetchLogin BillingView

--- src/app/dashboard/billing/components/BillingView.tsx
+++ src/app/dashboard/billing/components/BillingView.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useRef } from "react";
 import { AppInput, AppButton, AppModal } from "@/app/components/DS/Index";
-import { fetchLogin } from "@/app/services/users";
+// import { fetchLogin } from "@/app/services/users";
 import UpgradePlan from "@/app/components/UI/UpgradePlan";
 
 export default function BillingView() {
@@ -9,23 +9,25 @@ export default function BillingView() {
   const [form, setForm]: any = useState({});
   const [loading, setLoading] = useState(false);
 
-  const login = () => {
-    setLoading(true);
-    fetchLogin(form.username, form.password)
-      .then((result) => {
-        if (result.User) {
-          setForm({});
-          if (typeof window != "undefined") {
-            window.localStorage.setItem("token", result.User.token);
-            window.localStorage.setItem("user", JSON.stringify(result.User));
-            window.location.href = "/dashboard";
-          }
-        }
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+  // const login = () => {
+  //   setLoading(true);
+  //   fetchLogin(form.username, form.password)
+  //     .then((result) => {
+  //       if (result.User) {
+  //         setForm({});
+  //         if (typeof window != "undefined") {
+  //           window.localStorage.setItem("token", result.User.token);
+  //           window.localStorage.setItem("user", JSON.stringify(result.User));
+  //           window.location.href = "/dashboard";
+  //         }
+  //       }
+  //     })
+  //     .finally(() => {
+  //       setLoading(false);
+  //     });
+  // };
+
+  const formAction = () => {};
 
   return (
     <div>
@@ -52,7 +54,7 @@ export default function BillingView() {
         ref={modalRef}
       >
         <div className="0">
-          <form action={login} className="flex flex-col gap-5">
+          <form action={formAction} className="flex flex-col gap-5">
             <AppInput
               type="text"
               value={form.creditCardNumber}
