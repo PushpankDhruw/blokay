bff7bb25510010cd0cf8c3f65ff2167a9cc784d0
judare <fosebadgame@gmail.com>
2024-05-01 12:24:27 -0500

    init middleware

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const isAdminRoute = (pathname: string) => {
+  return pathname.startsWith("/api/admin");
+};
+
+const isUserRoute = (pathname: string) => {
+  return pathname.startsWith("/api/users");
+};
+
+export async function middleware(req: NextRequest) {
+  const role = req.headers.get("authorization") || "";
+  const { pathname } = req.nextUrl;
+
+  if (isUserRoute(pathname) && !["user", "admin"].includes(role)) {
+    return NextResponse.redirect(new URL("/api/auth/unauthorized", req.url));
+  }
+
+  if (isAdminRoute(pathname) && role !== "admin") {
+    return NextResponse.redirect(new URL("/api/auth/unauthorized", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/api/users/:path*",
+    "/api/brain/:path*",
+    "/api/datasources/:path*",
+  ],
+};
